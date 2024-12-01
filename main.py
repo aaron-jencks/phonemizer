777200b3167bbfff12de15a328ab5ed780911a3c
aaron-jencks <aaronjencks@aol.com>
2024-12-01 11:00:11 -0500

    created simplified main

---
+++ main.py
@@ -0,0 +1,27 @@
+import argparse
+import pathlib
+
+from phonemizer import phonemize, separator, version, logger, punctuation
+from phonemizer.backend import BACKENDS
+
+def main():
+    ap = argparse.ArgumentParser(description="takes an input string and converts it to phonemes")
+    ap.add_argument('input', type=str, help='the string to convert')
+    ap.add_argument('-l', '--library',
+                    type=pathlib.Path,
+                    default=pathlib.Path('../espeak-ng/build/lib/libespeak-ng.so'),
+                    help='the location of the libespeak-ng.so')
+    args = ap.parse_args()
+
+    BACKENDS['espeak'].set_library(args.library)
+    try:
+        espeak_library = BACKENDS['espeak'].library()
+    except RuntimeError:  # pragma: nocover
+        espeak_library = None
+
+    if espeak_library is None:
+        print('failed to load espeak library')
+        exit(1)
+
+    out = phonemize(args.input.splitlines())
+    print(out)
