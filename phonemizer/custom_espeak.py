23220f34ad292799cce6fe40b76150457538363e
aaron-jencks <aaronjencks@aol.com>
2025-05-30 13:34:23 -0400

    fixed substitution issues

--- phonemizer/custom_espeak.py
+++ phonemizer/custom_espeak.py
@@ -1,17 +1,31 @@
 from logging import Logger
-from re import Pattern
 import re
-from typing import List, Optional, Tuple, Union
+from typing import List, Optional, Union, Dict
 
 from phonemizer.backend.espeak.espeak import EspeakBackend
 from phonemizer.backend.espeak.language_switch import LanguageSwitch
 from phonemizer.backend.espeak.words_mismatch import WordMismatch
 
 
+def int_to_token(i: int, vocab: str) -> str:
+    if i < 0:
+        raise ValueError("Only non-negative integers are allowed.")
+    base = len(vocab)
+    if i < base:
+        return vocab[i]
+    result = []
+    while i >= base:
+        result.append(vocab[i % base])
+        i = i // base - 1  # subtract 1 to avoid leading 'a' being reused early
+    result.append(vocab[i])
+    return ''.join(reversed(result))
+
+
 class CustomEspeakBackend(EspeakBackend):
     def __init__(self, language: str,
                  punct: Optional[str] = None,
                  preserve_regex: Optional[List[str]] = None,
+                 token_vocab: str = ',`~',
                  preserve_punctuation: bool = False,
                  with_stress: bool = False,
                  tie: Union[bool, str] = False,
@@ -19,8 +33,11 @@ class CustomEspeakBackend(EspeakBackend):
                  words_mismatch: WordMismatch = 'ignore',
                  logger: Optional[Logger] = None):
         self.token_index = 0
+        self.token_vocab = token_vocab
+        self.token_prefix = '<<<{}>'
+        self.token_suffix = '</{}>>>'
         self.regex = re.compile('|'.join([f'({pattern})' for pattern in preserve_regex])) if preserve_regex else None
-        self.mappings = {}
+        self.mappings: Dict[str, int] = {}
 
         super().__init__(
             language,
@@ -45,30 +62,22 @@ class CustomEspeakBackend(EspeakBackend):
             phonemized = super(CustomEspeakBackend, self).phonemize([pre_process])[0]
             post_txt = self.post_process(phonemized)
             result.append(post_txt)
-            
+
         return result
 
     def pre_process(self, txt: str, phonemize):
         def replace_match(match):
             txt = match.group(0)
-            # token = self.token.format(
-            #     index=self.token_index,
-            #     content=txt
-            # )
-            # self.token_index += 1
-            token = f'<|.<|{self.token_index}|> {txt}.|>'
-            self.token_index += 1
 
-            # def mark_case(txt) -> str:
-            #     return '<>' + txt.group(0) + '<>'
-
-            # token = re.sub(r'[A-Z]', mark_case, token)
-            
             if txt in self.mappings:
+                encoded_token = int_to_token(self.mappings[txt], self.token_vocab)
+                token = f'{self.token_prefix.format(encoded_token)} {txt} {self.token_suffix.format(encoded_token)}'
                 return token
-                
-            phoneme = phonemize([token])[0]
-            self.mappings[txt] = phoneme.strip()
+
+            encoded_token = int_to_token(self.token_index, self.token_vocab)
+            token = f'{self.token_prefix.format(encoded_token)} {txt} {self.token_suffix.format(encoded_token)}'
+            self.mappings[txt] = self.token_index
+            self.token_index += 1
 
             return token
 
@@ -78,10 +87,16 @@ class CustomEspeakBackend(EspeakBackend):
 
     def post_process(self, phoneme: str):
         for token in self.mappings:
-            tphoneme = self.mappings[token]
-            if tphoneme not in phoneme:
-                raise Exception(f"replacement '{tphoneme}' not found in '{phoneme}'")
-            phoneme = phoneme.replace(
-                tphoneme, token)
+            index = self.mappings[token]
+            index_str = int_to_token(index, self.token_vocab)
+            prefix = self.token_prefix.format(index_str)
+            suffix = self.token_suffix.format(index_str)
+
+            if prefix not in phoneme:
+                raise Exception(f'phoneme {prefix} not in {phoneme}')
+            if suffix not in phoneme:
+                raise Exception(f'phoneme {suffix} not in {phoneme}')
+
+            phoneme = re.sub(f'{prefix} .*? {suffix}', token, phoneme)
 
         return phoneme
